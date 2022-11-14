8ddd73176e7c2ebfd9610d40125d196f068741ac
skwinnik <screen0994@gmail.com>
2022-11-14 20:05:41 +0000

    add external services

---
+++ .helm/install-external.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+helm upgrade -i postgresql bitnami/postgresql -f postgre/values.yaml --namespace async-course
+helm upgrade -i kafka bitnami/kafka -f kafka/values.yaml --namespace async-course
+helm upgrade -i kafka-ui kafka-ui -f kafka-ui/values.yaml --namespace async-course
---
+++ .helm/uninstall-external.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+helm uninstall postgresql kafka kafka-ui --namespace async-course
