51750535286f2e1655da0e37888d5b83259514cd
skwinnik <screen0994@gmail.com>
2022-11-15 09:30:29 +0000

    add pgadmin

--- .helm/install-external.sh
+++ .helm/install-external.sh
@@ -3,3 +3,4 @@
 helm upgrade -i postgresql bitnami/postgresql -f postgre/values.yaml --namespace async-course
 helm upgrade -i kafka bitnami/kafka -f kafka/values.yaml --namespace async-course
 helm upgrade -i kafka-ui kafka-ui -f kafka-ui/values.yaml --namespace async-course
+helm upgrade -i pgadmin runix/pgadmin4 -f pgadmin/values.yaml --namespace async-course
