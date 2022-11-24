f77746a960815f95e65a820bbc45201e3493c5e3
skwinnik <screen0994@gmail.com>
2022-11-24 18:53:56 +0000

    alter auth-service response

--- auth-service/src/auth/dto/jwt.dto.ts
+++ auth-service/src/auth/dto/jwt.dto.ts
@@ -1,9 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class JwtDto {
-  constructor(access_token: string) {
+export class LoggedInDto {
+  constructor(id: number, name: string, role: string, access_token: string) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
     this.access_token = access_token;
   }
+
   @ApiProperty({ example: 'string', description: 'encrypted JWT payload' })
   access_token: string;
+
+  @ApiProperty({ example: 1, description: 'user id' })
+  id: number;
+
+  @ApiProperty({ example: 'string', description: 'user name' })
+  name: string;
+
+  @ApiProperty({ example: 'string', description: 'user role' })
+  role: string;
 }
