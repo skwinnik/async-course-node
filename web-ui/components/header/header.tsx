9f20a5df6459f3477da7a0c7a45ca5c031f061cb
skwinnik <screen0994@gmail.com>
2022-11-26 07:03:21 +0000

    add tls support

--- web-ui/components/header/header.tsx
+++ web-ui/components/header/header.tsx
@@ -9,7 +9,7 @@ const Header: NextPage = () => {
       <div className="flex-1 text-right">
         {status === "loading" && "Loading!"}
         {status === "unauthenticated" && (
-          <div className="flex gap-5 items-center justify-between">
+          <div className="flex-1 gap-5 items-center justify-between">
             <div className="flex-auto flex-grow-0 flex-shrink-0">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
