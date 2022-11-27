e8444bc2cd682ca89ecf5a49dfc354ff87af6ec7
skwinnik <screen0994@gmail.com>
2022-11-27 08:00:09 +0000

    add guards to transaction controller

--- accounting-service/src/transactions/transactions.controller.ts
+++ accounting-service/src/transactions/transactions.controller.ts
@@ -6,8 +6,9 @@ import {
   ParseIntPipe,
   Req,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
-import { Ctx, EventPattern, KafkaContext } from '@nestjs/microservices';
+import { EventPattern } from '@nestjs/microservices';
 import { ValidationSchemaPipe } from 'src/pipes/validationSchema.pipe';
 import {
   TaskCompletedV1Event,
@@ -16,13 +17,20 @@ import {
 import { TransactionsService } from './transactions.service';
 import { Request } from 'express';
 import { TransactionDto } from './dto/transaction.dto';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { HasRoles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+import { Public } from 'src/auth/guards/public.attribute';
 
 @Controller('transactions')
+@ApiTags('Transactions')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @EventPattern('task.assigned.v1')
+  @Public()
   async taskAssigned(
     @Body(ValidationSchemaPipe<TaskAssignedV1Event>)
     taskAssignedV1Event: TaskAssignedV1Event,
@@ -35,6 +43,7 @@ export class TransactionsController {
   }
 
   @EventPattern('task.completed.v1')
+  @Public()
   async taskCompleted(
     @Body(ValidationSchemaPipe<TaskCompletedV1Event>)
     taskCompletedV1Event: TaskCompletedV1Event,
@@ -47,13 +56,13 @@ export class TransactionsController {
   }
 
   @Get('/all/:userId')
-  @ApiResponse({ type: Array<TransactionDto> })
+  @HasRoles('user')
   async getTransactions(
     @Req() req: Request,
     @Param('userId', ParseIntPipe) userId: number,
   ) {
     if (!req.user || req.user.id !== userId) throw new UnauthorizedException();
-    
+
     return (
       await this.transactionsService.findAll({
         where: { userId: userId },
